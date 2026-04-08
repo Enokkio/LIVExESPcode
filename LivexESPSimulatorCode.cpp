d5472e5386dba4ae3ff8dbb9ce2d5c0e52444967
Enokkio <102516217+Enokkio@users.noreply.github.com>
2026-04-08 22:16:54 +0200

    added ble code ...

--- LivexESPSimulatorCode.cpp
+++ LivexESPSimulatorCode.cpp
@@ -63,3 +63,17 @@ void loop() {
 
   delay(2000); // Send every 2 seconds
 }
+
+void sendBLE(uint8_t id, float lat, float lon) {
+  uint8_t buffer[9];
+  buffer[0] = id;
+  memcpy(buffer + 1, &lat, sizeof(float));
+  memcpy(buffer + 5, &lon, sizeof(float));
+
+  pCharacteristic->setValue(buffer, sizeof(buffer));
+  pCharacteristic->notify();
+
+  Serial.print("BLE sent ID "); Serial.print(id);
+  Serial.print(" -> Lat: "); Serial.print(lat, 6);
+  Serial.print(", Lon: "); Serial.println(lon, 6);
+}
